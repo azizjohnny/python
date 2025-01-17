22bf9a05a1e60d02a7af5d7bddb9769f84f8a828
Parzivald <bez@Muhammadqodirs-Mac.local>
2025-01-17 13:58:23 +0500

    check_clean

--- AzizFiverrXnarx-main/check_clean.py
+++ AzizFiverrXnarx-main/check_clean.py
@@ -1,26 +1,7 @@
 import re
+from Dataclean import process_product_name
 
-product_name = "Samsung Galaxy S21 Fe White"
+product_name = "Смартфон Oppo A18 4/128 Glowing Blue"
 product_name_lower = product_name.lower()
 
-def check_clean(product_name):
-    colors = ["black", "silver", "gold", "gray", "grey", "blue", "green", "white", 
-                "space gray", "space grey", "midnight", "mint", "minty", "lavender"]
-    if any(color in product_name_lower for color in colors):
-        # First handle compound colors with spaces
-        product_name = re.sub(
-            r'\s+space\s+gr[ae]y\s*',
-            ' ',
-            product_name,
-            flags=re.IGNORECASE
-        ).strip()
-        # Then handle single-word colors
-        product_name = re.sub(
-            r'\s+(black|silver|gold|gr[ae]y|blue|green|white|midnight|mint|lavender)\s*',
-            ' ',
-            product_name,
-            flags=re.IGNORECASE
-        ).strip()
-    return product_name
-
-print(check_clean(product_name))
+print(process_product_name(product_name, "Smartfonlar"))
